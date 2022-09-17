abef2e7792c82837a1e7d7d2ba5ffd5d2ef160dd
Samuel Yvon <samuelyvon9@gmail.com>
2022-09-17 15:33:10 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# rs-spotify-status
+
+> Configurable and easy to use Spotify status for i3bar 
+
+## Why?
+
+I see a lot of configuration with a simple script like this to display the current 
+song playing on spotify in the i3bar:
+
+```shell
+spotify_info=$(dbus-send --print-reply \
+    --dest=org.mpris.MediaPlayer2.spotify \
+    /org/mpris/MediaPlayer2 \
+    org.freedesktop.DBus.Properties.Get \
+    string:org.mpris.MediaPlayer2.Player \
+    string:Metadata 
+)
+title=$(echo "$spotify_info" | sed -n '/title/{n;p}' | cut -d '"' -f 2)
+artist=$(echo "$spotify_info" | grep artist -A 2 | tail -n 1 | cut -d '"' -f 2)
+
+if [[ "$title" != "" ]]; then
+    echo "<span color=\"white\">&#xf1bc; $title ($artist)</span>"
+fi
+```
+
+This works 99.99% of the time, but not always. It fails on songs with multiple artists
+or titles that are absurdly long. Also, it's not in Rust, so it's not blazingly fast (/s).
+
+## Configuration
+
+Configuration can be provided through a TOML file at `~/.spotify-status`. An example
+configuration follows:
+
+```TOML
+icon = # icon string to print. By default, uses the font awesome character code for the spotify icon
+color = # the color of the text. By default, white is used.
+max_length = # The max length of the string output. By default, 45 UTF-8 characters.
+```
+
---
+++ src/main.rs
@@ -0,0 +1,138 @@
+use dbus::arg::{prop_cast, PropMap};
+use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
+use dbus::blocking::Connection;
+use serde::Deserialize;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::time::Duration;
+
+const CONFIG_FILE_NAME: &str = ".spotify-status";
+
+const SPOTIFY_DBUS_DEST: &str = "org.mpris.MediaPlayer2.spotify";
+const MEDIA_INTERFACE_PATH: &str = "/org/mpris/MediaPlayer2";
+const MPRIS_MEDIA_INTERFACE: &str = "org.mpris.MediaPlayer2.Player";
+const MEDIA_METADATA_PROP: &str = "Metadata";
+const TITLE_PROPERTY: &str = "xesam:title";
+const ARTISTS_PROPERTY: &str = "xesam:artist";
+
+const ERR_NO_HOME_DIR: &str = "Error; could not find the home directory of the current user";
+const ERR_UNABLE_TO_OPEN_CONFIG_FILE_BUT_EXISTS: &str =
+    "Error; unable to open the config file but it exists";
+
+const SPOTIFY_ICON_AWESOME_FONTS: &str = "&#xf1bc;";
+const DEFAULT_COLOR: &str = "white";
+const DEFAULT_MAX_LENGTH: usize = 45;
+
+#[derive(Deserialize)]
+struct Config {
+    icon: Option<String>,
+    color: Option<String>,
+    max_length: Option<usize>,
+}
+
+impl Config {
+    /// Default configuration
+    fn default() -> Config {
+        Config {
+            icon: Some(SPOTIFY_ICON_AWESOME_FONTS.to_string()),
+            color: Some(DEFAULT_COLOR.to_string()),
+            max_length: Some(DEFAULT_MAX_LENGTH),
+        }
+    }
+}
+
+fn resolve_config() -> std::result::Result<Config, Box<dyn std::error::Error>> {
+    let home_dir = home::home_dir();
+
+    if home_dir.is_none() {
+        return Err(ERR_NO_HOME_DIR)?;
+    }
+
+    let config_path = home_dir.unwrap().join(CONFIG_FILE_NAME);
+
+    if !config_path.exists() {
+        return Ok(Config::default());
+    }
+
+    let file = File::open(config_path).expect(ERR_UNABLE_TO_OPEN_CONFIG_FILE_BUT_EXISTS);
+
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+
+    reader.read_to_string(&mut contents)?;
+
+    let config: Config = toml::from_str(&contents)
+        .map_err(|err| -> String { format!("Failed to parse the configuration file: {err}") })?;
+
+    Ok(config)
+}
+
+#[test]
+fn test_trim_to_length_short() -> Result<(), String> {
+    let less_than_30 = "hello";
+    assert_eq!(trim_to_length(less_than_30, 30).len(), 5);
+    Ok(())
+}
+
+#[test]
+fn test_trim_to_length_limit() -> Result<(), String> {
+    let exactly_30 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    assert_eq!(trim_to_length(exactly_30, 30).len(), 30);
+    Ok(())
+}
+
+#[test]
+fn test_trim_to_length_above_limit() -> Result<(), String> {
+    let more_than_30 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    assert_eq!(trim_to_length(more_than_30, 30).len(), 30);
+    Ok(())
+}
+
+fn trim_to_length(input: &str, max_length: usize) -> String {
+    let original_str_len = input.len();
+
+    if original_str_len <= max_length {
+        return String::from(input);
+    }
+
+    let diff = original_str_len - max_length + 3;
+    let mid_ish = original_str_len / 2;
+
+    let pre = &input[..mid_ish - diff / 2];
+    let post = &input[mid_ish + diff / 2..];
+
+    format!("{pre}...{post}")
+}
+
+fn format_for_printing(config: &Config, display_str: &str) -> String {
+    let icon = config.icon.as_deref().unwrap_or(SPOTIFY_ICON_AWESOME_FONTS);
+    let color = config.color.as_deref().unwrap_or(DEFAULT_COLOR);
+    let max_length = config.max_length.unwrap_or(DEFAULT_MAX_LENGTH);
+
+    let sized_display_str = trim_to_length(display_str, max_length);
+
+    return format!("<span color=\"{color}\">{icon} {sized_display_str}</span>");
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let conn = Connection::new_session()?;
+
+    let spotify_dbus_proxy = conn.with_proxy(
+        SPOTIFY_DBUS_DEST,
+        MEDIA_INTERFACE_PATH,
+        Duration::from_millis(5000),
+    );
+
+    let config = resolve_config()?;
+
+    let metadata: PropMap = spotify_dbus_proxy.get(MPRIS_MEDIA_INTERFACE, MEDIA_METADATA_PROP)?;
+
+    let title: &String = prop_cast(&metadata, TITLE_PROPERTY).unwrap();
+    let artists: &Vec<String> = prop_cast(&metadata, ARTISTS_PROPERTY).unwrap();
+
+    let contents = format!("{title} (by {})", artists[0]);
+
+    print!("{}", format_for_printing(&config, &contents));
+
+    Ok(())
+}
